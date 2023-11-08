b7e6f0e9d1fe18f2fdd876eec0b1927e8ab57c7a
Luke Wheldale <lukewheldale@gmail.com>
2023-11-08 20:19:13 +1030

    feat: added total exercises

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 const Header = ({ course }) => <h1>{course}</h1>;
 
-const Total = ({ sum }) => <p>Number of exercises {sum}</p>;
+const Total = ({ sum }) => <h4>Total of {sum} exercises </h4>;
 
 const Part = ({ part }) => (
 	<p>
@@ -16,39 +16,73 @@ const Content = ({ parts }) => (
 	</>
 );
 
-const Course = ({ course }) => {
+const Course = ({ courses }) => {
 	return (
 		<>
-			<Header course={course.name} />
-			<Content parts={course.parts} />
+			<Header course={courses[0].name} />
+			<Content parts={courses[0].parts} />
+			<Total
+				sum={
+					courses[0].parts[0].exercises +
+					courses[0].parts[1].exercises +
+					courses[0].parts[2].exercises
+				}
+			/>
 		</>
 	);
 };
 
 const App = () => {
-	const course = {
-		id: 1,
-		name: "Half Stack application development",
-		parts: [
-			{
-				name: "Fundamentals of React",
-				exercises: 10,
-				id: 1,
-			},
-			{
-				name: "Using props to pass data",
-				exercises: 7,
-				id: 2,
-			},
-			{
-				name: "State of a component",
-				exercises: 14,
-				id: 3,
-			},
-		],
-	};
+	const courses = [
+		{
+			name: "Half Stack application development",
+			id: 1,
+			parts: [
+				{
+					name: "Fundamentals of React",
+					exercises: 10,
+					id: 1,
+				},
+				{
+					name: "Using props to pass data",
+					exercises: 7,
+					id: 2,
+				},
+				{
+					name: "State of a component",
+					exercises: 14,
+					id: 3,
+				},
+				{
+					name: "Redux",
+					exercises: 11,
+					id: 4,
+				},
+			],
+		},
+		{
+			name: "Node.js",
+			id: 2,
+			parts: [
+				{
+					name: "Routing",
+					exercises: 3,
+					id: 1,
+				},
+				{
+					name: "Middlewares",
+					exercises: 7,
+					id: 2,
+				},
+			],
+		},
+	];
 
-	return <Course course={course} />;
+	return (
+		<div>
+			<Course courses={courses} />
+		</div>
+	);
 };
 
 export default App;
